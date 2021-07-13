737e6e3a3c9194366c6f6e0f5d95fe25447d384a
Arief Syahroni <riefsyah31@gmail.com>
2021-07-13 22:48:23 +0700

    Add some file and assets for development

---
+++ lib/theme.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color backgroundColor = Color(0XFF252836);
+Color purpleColor = Color(0XFF6A6AE3);
+Color whiteColor = Color(0XFFFFFFFF);
+Color greyColor = Color(0XFF9A9CA5);
+
+final double defaultMargin = 24.0;
+
+TextStyle titleTextStyle = GoogleFonts.poppins(
+  fontWeight: FontWeight.w600,
+  color: whiteColor,
+);
+
+TextStyle subTitleTextStyle = GoogleFonts.poppins(
+  fontWeight: FontWeight.w400,
+  color: greyColor,
+);
