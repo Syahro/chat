eaadf78ef163f65a41f8a56c93052ef7a42626b0
Arief Syahroni <riefsyah31@gmail.com>
2021-07-13 23:05:14 +0700

    done Modified SplashScreen

--- lib/pages/splash_screen.dart
+++ lib/pages/splash_screen.dart
@@ -1,13 +1,68 @@
+import 'package:chat/theme.dart';
 import 'package:flutter/material.dart';
 
 class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: backgroundColor,
       body: SingleChildScrollView(
         scrollDirection: Axis.vertical,
-        child: Column(
-          children: [],
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: defaultMargin,
+            vertical: 130,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                width: 348,
+                height: 285,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage(
+                      'assets/illustration.png',
+                    ),
+                  ),
+                ),
+              ),
+              Text(
+                'instant messaging, simple\nAnd personal',
+                style: titleTextStyle.copyWith(fontSize: 24),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                'start your new journey in the Chat Us',
+                style: subTitleTextStyle.copyWith(
+                  fontSize: 14,
+                ),
+              ),
+              SizedBox(
+                height: 25,
+              ),
+              Container(
+                height: 55,
+                width: 180,
+                child: RaisedButton(
+                  onPressed: () {},
+                  color: purpleColor,
+                  child: Text(
+                    'Let\'s Begin',
+                    style: titleTextStyle.copyWith(
+                      fontSize: 20,
+                      color: whiteColor,
+                    ),
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
